893de1e4d503a0ba72d932eb6209e9c09f8e07bf
fedeit <fede.galbi@hotmail.com>
2020-12-27 21:50:04 +0100

    Created an initialize function for discord

--- src/discord_integration.js
+++ src/discord_integration.js
@@ -1,21 +1,27 @@
 const Discord = require('discord.js');
 // Vars to be defined later
 var guild;
-// Add intent for retrieving discord guild
-let intents = new Discord.Intents(Discord.Intents.NON_PRIVILEGED);
-intents.add('GUILD_MEMBERS');
-// Add intent to client
-const client = new Discord.Client({ ws: {intents: intents}});
-// Login using token
-client.login(process.env.DISCORD_BOT_TOKEN);
+var client;
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  // Connect to the Guild
-  guild = client.guilds.cache.get(process.env.GUILD)
-  // Get list of members
-  guild.members.fetch({ cache: false })
-  .then(console.log)
-  .catch(console.error);
-});
+exports.initialize = () => {
+	// Add intent for retrieving discord guild
+	let intents = new Discord.Intents(Discord.Intents.NON_PRIVILEGED);
+	intents.add('GUILD_MEMBERS');
+	// Add intent to client
+	client = new Discord.Client({ ws: {intents: intents}});
+	// Login using token
+	client.login(process.env.DISCORD_BOT_TOKEN);
 
+	client.on('ready', () => {
+	  console.log(`Logged in as ${client.user.tag}!`);
+	  // Get the guild
+	  guild = client.guilds.cache.get(process.env.GUILD);
+	});
+}
+
+exports.getMembers = (callback) => {
+	// Get list of members
+	guild.members.fetch({ cache: false })
+	.then(callback)
+	.catch(console.error);
+}
